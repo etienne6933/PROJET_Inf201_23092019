4879d58d15e6456435225d39d8bfe0699531b99d
Admin <Admin@DESKTOP-MR3VSJS>
2019-09-21 19:14:21 +0200

    Suite le 21092019

--- Projet2309tuto/src/Main.java
+++ Projet2309tuto/src/Main.java
@@ -5,28 +5,44 @@ import java.util.Scanner;
 public class Main {
 	//La fonction main
 	public static void main(String[] args) throws Exception {
-		//créer un objet qui appelle la classe SQL on la nommera base
+		//créer un nouvel objet qui appelle la classe SQL on la nommera base
 		SQL base = new SQL();
 		//ajouter l'entrée clavier pour dire qu'on va lire l'entrée clavier
 		Scanner sc = new Scanner(System.in);
-		//new variable entier
+		System.out.println("Choisir une option: \n a. Pour afficher les différents séjours d’un patient : taper 1 ");
+		System.out.println(" b. Pour afficher le Nombre d’actes subis par patient sur une période : taper 2 ");
+		System.out.println(" c. Pour afficher les diagnostics les plus fréquents chez les femmes : taper 3 ");
+		System.out.println(" d. Pour afficher les patients ayant eu le plus d’hospitalisations  : taper 4 ");
+		System.out.println(" e. Pour afficher les patients âgés de plus de 80 ans  : taper 5 ");
+		System.out.println(" f. Pour mettre à jour le sexe d'un patient  : taper 6 ");
+		
+		//On crée une variable entier
 		int choix = sc.nextInt();
-		//on lui demande d'imprimer choix concatené au caractère tapé
+		//On demande d'afficher le choix concatené au caractère tapé récupéré dans Scanner	
 		System.out.println("Choix: " + choix);
 		switch(choix){
 		//en rouge les fonctions systeme en vert les fonctions que je viens de créer
 		//les fonctions de base comme case sont tout de suite reconnues
 		//les fonctions que je crée comme sejours ou nbActes ne sont pas tout se suite reconnues
 		//tant qu'elles n'existent pas dans la classe SQL
-		//on va utiliser case pour tester 2 cas avec une sortie break à chaque fois 
-		//pour ne pas saisir le code SQL dans main on appelle depuis main uniquement la fonction sejour de la classe SQL qui contient le code
+		//on va utiliser case pour tester x cas avec une sortie break à chaque fois 
+		//pour ne pas saisir le code SQL dans main on appelle depuis main uniquement la fonction sejour par exemple de la classe SQL qui contient le code
+						
 		case 1: base.sejours();
 		break;
-		//pour le nom toujours 1er mot min au début et 2 eme MAJ au debut
 		case 2: base.nbActes();
 		break;
-		//le default est équivalent au else ici si aucun choix connu
-		default: System.out.println("Erreur de saisie");
+		case 3: base.freqdiag();
+		break;
+		case 4: base.hospitmaxi();
+		break;
+		case 5: base.pat80etplus();
+		break;
+		case 6: base.majsexe();
+		break;
+		
+		//le default est équivalent au else ici si aucun choix connu tapé par l'utilisateur
+		default: System.out.println("Erreur de saisie, choisissez un des choix disponibles!");
 		}
 	}
 
--- Projet2309tuto/src/SQL.java
+++ Projet2309tuto/src/SQL.java
@@ -1,9 +1,13 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.Scanner;
 
 public class SQL {
+	private Connection myConn;
 	//on créé une fonction type statement nommée connexion
 	public Statement connexion() throws Exception {
 		//le try pour essayer ce code
@@ -17,23 +21,26 @@ public class SQL {
 			Statement st= con.createStatement();
 			//le return est quasi booleen pour dire si la connexion est Ok ou pas
 			return st;
-			//avec le catch on attrape l'exception et l'imprime
+			//avec le catch on attrape l'exception et on l'affiche
 		} catch (Exception e) {
 			throw e;
 		}
 
 	}
-	
-	
+		
 	//fonction sejours qui est appelée dans Main
 	public void sejours() throws Exception {
 		try {
 			Statement st = connexion();
-			ResultSet rs = st.executeQuery("select * from tab_patient P");
+			ResultSet rs = st.executeQuery("select T_P.NOM, T_P.PRENOM, T_H.ID_HOSPITALISATION, T_H.DATE_ENTREE, T_H.DATE_SORTIE FROM tab_hospitalisation T_H INNER JOIN tab_patient T_P ON T_H.ID_PATIENT=T_P.ID_PATIENT ORDER BY `T_P`.`NOM` ASC");
 			while (rs.next()) {
-				int ID_PATIENT = rs.getInt(1);
-				String SEXE = rs.getString(2);
-				System.out.println("ID_P = " + ID_PATIENT + " " + "SEXE_P =" + SEXE);
+				String NOM = rs.getString(1);
+				String PRENOM = rs.getString(2);
+				int ID_HOSPITALISATION = rs.getInt(3);
+				//attention date de sortie peut être null créer une exception
+				Date DATE_ENTREE = rs.getDate(4);
+				Date DATE_SORTIE = rs.getDate(5);
+				System.out.println("Nom = " + NOM + "||" + "Prenom = " + PRENOM + "||"+"ID_HOSP = " + ID_HOSPITALISATION + "||" + "DATE_ENTREE = " + DATE_ENTREE+ "||" + "DATE_SORTIE = " + DATE_SORTIE);
 			}
 
 		} catch (Exception e) {
@@ -46,10 +53,23 @@ public class SQL {
 	public void nbActes() throws Exception {
 		try {
 			Statement st = connexion();
-			ResultSet rs = st.executeQuery("select count(*) as total from tab_patient");
+			ResultSet rs = st.executeQuery("select count(DISTINCT T_A.ID_AKT) AS NB_ACTES, T_H.ID_HOSPITALISATION, T_H.ID_PATIENT, T_P.NOM, T_P.PRENOM \r\n" + 
+					"from tab_acte T_A \r\n" + 
+					"INNER JOIN tab_hospitalisation T_H ON T_A.ID_HOSPITALISATION=T_H.ID_HOSPITALISATION \r\n" + 
+					"INNER JOIN tab_patient T_P ON T_H.ID_PATIENT=T_P.ID_PATIENT  \r\n" + 
+					"WHERE T_H.DATE_ENTREE BETWEEN '2004-01-01' AND '2004-12-31'\r\n" + 
+					"GROUP BY ID_PATIENT  \r\n" + 
+					"ORDER BY `T_H`.`ID_PATIENT` ASC\r\n" + 
+					"");
 			while (rs.next()) {
-				int total = rs.getInt("total");
-				System.out.println("Total = " + total);
+				int NB_ACTES = rs.getInt(1);
+				int ID_HOSPITALISATION = rs.getInt(2);
+				int ID_PATIENT = rs.getInt(3);
+				String NOM = rs.getString(4);
+				String PRENOM = rs.getString(5);
+				
+				
+				System.out.println("Total des actes = " + NB_ACTES + "||"+ "ID_HOSP = " + ID_HOSPITALISATION + "||"+ "NOM_Patient = " + PRENOM + "||"+ "PRENOM_Patient = " + PRENOM);
 			}
 
 		} catch (Exception e) {
@@ -57,4 +77,105 @@ public class SQL {
 		}
 
 	}
+	//fonction freqdiag qui est appelée dans Main
+		public void freqdiag() throws Exception {
+			try {
+				Statement st = connexion();
+				ResultSet rs = st.executeQuery("SELECT   COUNT(CODE_CIM10) AS NOMBRE_DE_CAS, CODE_CIM10, LIBELLE_CIM110, SEXE AS SEXE_PATIENT\r\n" + 
+						"FROM     tab_diagnostic\r\n" + 
+						"INNER JOIN tab_cim10 ON tab_diagnostic.CODE_CIM10 = tab_cim10.ID_CIM10\r\n" + 
+						"INNER JOIN tab_hospitalisation ON tab_diagnostic.ID_HOSPITALISATION = tab_hospitalisation.ID_HOSPITALISATION\r\n" + 
+						"INNER JOIN tab_patient ON tab_hospitalisation.ID_PATIENT = tab_patient.ID_PATIENT\r\n" + 
+						"WHERE tab_patient.SEXE = '2'\r\n" + 
+						"GROUP BY CODE_CIM10\r\n" + 
+						"HAVING   COUNT(CODE_CIM10) > 1  \r\n" + 
+						"ORDER BY `NOMBRE_DE_CAS`  DESC\r\n" + 
+						"");
+				while (rs.next()) {
+					int NOMBRE_DE_CAS = rs.getInt(1);
+					String CODE_CIM10 = rs.getString(2);
+					String LIBELLE_CIM110 = rs.getString(3);
+					int SEXE_PATIENT = rs.getInt(4);
+										
+					System.out.println("NOMBRE_DE_CAS = " + NOMBRE_DE_CAS + "||"+ "CODE_CIM10 = " + CODE_CIM10 + "||"+ "LIBELLE_CIM10 = " + LIBELLE_CIM110+ "||"+ "SEXE_PATIENT = " + SEXE_PATIENT);
+				}
+
+			} catch (Exception e) {
+				throw e;
+			}
+
+		}
+		//fonction hospitmaxi qui est appelée dans Main
+		public void hospitmaxi() throws Exception {
+			try {
+				Statement st = connexion();
+				ResultSet rs = st.executeQuery("SELECT count(distinct ID_HOSPITALISATION) AS NOMBRE_HOSPIT,tab_hospitalisation.ID_PATIENT,NOM,PRENOM,DATE_NAISSANCE  from tab_hospitalisation\r\n" + 
+						"INNER JOIN tab_patient ON tab_hospitalisation.ID_PATIENT = tab_patient.ID_PATIENT\r\n" + 
+						"GROUP BY ID_PATIENT\r\n" + 
+						"ORDER BY `NOMBRE_HOSPIT`  DESC\r\n" + 
+						"");
+				while (rs.next()) {
+					int NOMBRE_HOSPIT = rs.getInt(1);
+					int ID_PATIENT = rs.getInt(2);
+					String NOM = rs.getString(3);
+					String PRENOM = rs.getString(4);
+					Date DATE_NAISSANCE = rs.getDate(5);
+										
+					System.out.println("NOMBRE_HOSPIT = " + NOMBRE_HOSPIT + "||"+ "ID_PATIENT = " + ID_PATIENT + "||"+ "NOM = " + NOM + "||"+ "PRENOM = " + PRENOM + "||"+ "DATE_NAISS = " + DATE_NAISSANCE);
+				}
+
+			} catch (Exception e) {
+				throw e;
+			}
+
+		}
+		//fonction pat80etplus qui est appelée dans Main
+		public void pat80etplus() throws Exception {
+			try {
+				Statement st = connexion();
+				ResultSet rs = st.executeQuery("select ID_PATIENT, NOM, PRENOM, DATE_NAISSANCE, SEXE from tab_patient where DATE_NAISSANCE < (SELECT CURDATE()- INTERVAL 29200 DAY)");
+				while (rs.next()) {
+					int ID_PATIENT = rs.getInt(1);
+					String NOM = rs.getString(2);
+					String PRENOM = rs.getString(3);
+					Date DATE_NAISSANCE = rs.getDate(4);
+					int SEXE = rs.getInt(5);
+										
+					System.out.println("ID_PATIENT = " + ID_PATIENT + "||"+ "NOM = " + NOM + "||"+ "PRENOM = " + PRENOM + "||"+ "DATE_NAISS = " + DATE_NAISSANCE + "||"+ "SEXE = " + SEXE );
+				}
+
+			} catch (Exception e) {
+				throw e;
+			}
+
+		}
+		//fonction majsexe qui est appelée dans Main
+		public void majsexe() throws Exception {
+			String sql = "update tab_patient set SEXE=? where ID_PATIENT=?";
+			System.out.println("Veuillez saisir un Identifiant pour le patient :");
+			Scanner sc2 = new Scanner(System.in);
+			String sc2idpat = sc2.next();
+			System.out.println("Vous avez saisi l'ID_PATIENT : " + sc2idpat);
+			System.out.println("Veuillez saisir un Sexe pour le patient 1=Homme 2=Femme :");
+			Scanner sc3 = new Scanner(System.in);
+			String sc3codesexe = sc3.next();
+			System.out.println("Vous avez saisi le code sexe : " + sc3codesexe);
+			try {
+				myConn = null;
+				Object myStmt = myConn.prepareStatement(sql); {
+					((PreparedStatement) myStmt).setInt(1, 2);
+					((PreparedStatement) myStmt).setInt(2, 100324);
+					((PreparedStatement) myStmt).executeUpdate();
+				
+				
+				
+															
+					System.out.println("MAJ faite");
+				}
+
+			} catch (Exception e) {
+				throw e;
+			}
+
+		}
 }
